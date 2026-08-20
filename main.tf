cb11b114ff7f68ad304f071e39efeab6847a2559
RIJU NANDI <it.20203012@gmail.com>
2026-08-20 12:59:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-data "aws_subnets" "available-subnets"{
-    filter {
-        name = "tag:Name"
-        values = ["Our-Public-*"]
-    }
+data "aws_subnets" "available-subnets" {
+  filter {
+    name   = "tag:Name"
+    values = ["Our-Public-*"]
+  }
 }
 
 resource "aws_eks_cluster" "project-cluster" {
@@ -13,7 +13,21 @@ resource "aws_eks_cluster" "project-cluster" {
     subnet_ids = data.aws_subnets.available-subnets.ids
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS resource "aws_eks_node_group" "node-grp" {
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.project-cluster.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.project-cluster.certificate_authority[0].data
+}
+
+resource "aws_eks_node_group" "node-grp" {
   cluster_name    = aws_eks_cluster.project-cluster.name
   node_group_name = "pc-node-group"
   node_role_arn   = aws_iam_role.worker.arn
@@ -43,44 +57,4 @@ resource "aws_eks_cluster" "project-cluster" {
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly
   ]
-}Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
-  ]
-}
-
-output "endpoint" {
-  value = aws_eks_cluster.project-cluster.endpoint
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.project-cluster.certificate_authority[0].data
-}
-
-resource "aws_eks_node_group" "node-grp" {
-  cluster_name    = aws_eks_cluster.project-cluster.name
-  node_group_name = "pc-node-group"
-  node_role_arn   = aws_iam_role.worker.arn
-  subnet_ids      = data.aws_subnets.available-subnets.ids
-  capacity_type   = "ON_DEMAND"
-  disk_size       = "40"
-  instance_types  = ["c7i-flex.large"]
-  labels = tomap({ env = "dev" })
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
-  }
-
-  update_config {
-    max_unavailable = 1
-  }
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly
-    ]  
 }
