7657317ee95135f1da029dbb700ac4f516cdc718
RIJU NANDI <it.20203012@gmail.com>
2026-08-20 12:57:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,37 @@ resource "aws_eks_cluster" "project-cluster" {
     subnet_ids = data.aws_subnets.available-subnets.ids
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Ensure that IAM Role permissions are created before and deleted after EKS resource "aws_eks_node_group" "node-grp" {
+  cluster_name    = aws_eks_cluster.project-cluster.name
+  node_group_name = "pc-node-group"
+  node_role_arn   = aws_iam_role.worker.arn
+
+  subnet_ids = data.aws_subnets.available-subnets.ids
+
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 40
+  instance_types = ["t3.medium"]
+
+  labels = tomap({
+    env = "dev"
+  })
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly
+  ]
+}Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
